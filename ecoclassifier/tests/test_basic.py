3b5cedfa44a86dfd3be15a3f6e5216d58f74b9ca
Pierre-Julien Grizel <pjgrizel@numericube.com>
2019-04-03 12:22:50 +0200

    Added basic tests and Travis.CI

---
+++ ecoclassifier/tests/test_basic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test_basic.py
+
+Created by Pierre-Julien Grizel et al.
+Copyright (c) 2016 NumeriCube. All rights reserved.
+
+Basic testing procedure
+"""
+from __future__ import unicode_literals
+
+__author__ = ""
+__copyright__ = "Copyright 2016, NumeriCube"
+__credits__ = ["Pierre-Julien Grizel"]
+__license__ = "CLOSED SOURCE"
+__version__ = "TBD"
+__maintainer__ = "Pierre-Julien Grizel"
+__email__ = "pjgrizel@numericube.com"
+__status__ = "Production"
+
+
+def test_basic():
+    """Just test that I can test
+    """
+    assert 1 == 1
