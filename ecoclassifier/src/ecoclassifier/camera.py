5815db9d37ec12fa5a715aa0bf7c91b7d1a343a3
Pierre-Julien Grizel <pjgrizel@numericube.com>
2019-06-25 13:36:46 +0200

    global camera config

--- ecoclassifier/src/ecoclassifier/camera.py
+++ ecoclassifier/src/ecoclassifier/camera.py
@@ -32,53 +32,69 @@ from . import settings
 
 logger = logging.getLogger(__name__)
 
+# Initialize cameras
+devices = None
+cameras = None
+tlFactory = py.TlFactory.GetInstance()
+devices = tlFactory.EnumerateDevices()
+cameras = py.InstantCameraArray(len(devices))
+for i, cam in enumerate(cameras):
+    cam.Attach(tlFactory.CreateDevice(devices[i]))
+    logger.debug("Using device %s", cam.GetDeviceInfo().GetIpAddress())
+cameras.StartGrabbing(py.GrabStrategy_LatestImageOnly)
+
 
 class Camera:
     """Camera abstraction
     """
 
-    cameras = None
+    # cameras = None
     cam_idx = 0
     ip = None
-    _cam = None
+    # _cam = None
 
-    def __init__(self, ip=None):
+    def __init__(self, ip):
         """Initialize a camera object, grab the first camera that matches the "authorized_fullnames"
         name.
         """
+        if ip == settings.CAMERA_VT_IP:
+            cam_idx = 0
+        else:
+            cam_idx = 1
+
         # Save parameters for later
         self.ip = ip
 
         # Get the transport layer factory.
-        self.tlFactory = py.TlFactory.GetInstance()
-        self.maxCamerasToUse = 2
+        # self.tlFactory = py.TlFactory.GetInstance()
 
         # Get all attached devices and exit application if no device is found.
-        self.devices = self.tlFactory.EnumerateDevices()
-        if len(self.devices) == 0:
-            raise IOError("No camera present.")
-        else:
-            logger.debug("%d cameras found.", len(self.devices))
+        # self.devices = self.tlFactory.EnumerateDevices()
+        # if len(self.devices) == 0:
+        #     raise IOError("No camera present.")
+        # else:
+        #     logger.debug("%d cameras found.", len(self.devices))
 
-        # Scan devices until our IP camera is found
-        cam_device = None
-        for device_info in self.devices:
-            if device_info.GetIpAddress() == ip:
-                logger.debug("Found camera on {}".format(ip))
-                self._cam_device = self.tlFactory.CreateDevice(device_info)
-                self._cam = py.InstantCamera(self._cam_device)
-        assert self._cam.GetDeviceInfo().GetIpAddress() == ip
-
-        # Let's start the fun
-        self._cam.StartGrabbing(py.GrabStrategy_LatestImageOnly)
+        # Connect camera
+        # device_info = py.DeviceInfo()
+        # device_info.SetIpAddress(ip)
+        # # for device_info in self.devices:
+        #     # if device_info.GetIpAddress() == ip:
+        #         # logger.debug("Found camera on {}".format(ip))
+        # self._cam_device = self.tlFactory.CreateDevice(device_info)
+        # self._cam = py.InstantCamera(self._cam_device)
+        # # assert self._cam.GetDeviceInfo().GetIpAddress() == ip
+        #
+        # # Let's start the fun
+        # self._cam.StartGrabbing(py.GrabStrategy_LatestImageOnly)
 
     def loadConf(self, path):
         """Load configuration file (NodeMap.pfs)"""
-        py.FeaturePersistence.Load(path, self._cam.GetNodeMap())
+        py.FeaturePersistence.Load(path, cameras[self.cam_idx].GetNodeMap())
 
     def saveConf(self, path):
         """Save camera status to NodeMap.pfs"""
-        py.FeaturePersistence.Save(path, self._cam.GetNodeMap())
+        py.FeaturePersistence.Save(path, cameras[self.cam_idx].GetNodeMap())
 
     def grabImage(self):
         """Grab an image from the camera, ONE-BY-ONE MODE"""
@@ -86,50 +102,48 @@ class Camera:
 
         # MUST set, TOTAL number of images to grab per camera !
         start_t = time.time()
-        countOfImagesToGrab = 1
         # Grab c_countOfImagesToGrab from the cameras.
-        for i in range(countOfImagesToGrab):
-            if not self._cam.IsGrabbing():
-                logger.debug("Camera is not in grabbing mode")
-                break
-            grabResult = self._cam.RetrieveResult(
-                5000, py.TimeoutHandling_ThrowException
-            )
-            # cameraContextValue = grabResult.GetCameraContext()
+        if not cameras[self.cam_idx].IsGrabbing():
+            logger.debug("Camera is not in grabbing mode")
+            return
+        grabResult = cameras[self.cam_idx].RetrieveResult(
+            5000, py.TimeoutHandling_ThrowException
+        )
+        # cameraContextValue = grabResult.GetCameraContext()
 
-            # Print the index and the model name of the camera.
-            # print("Camera ", cameraContextValue, ": ", self.cameras[cameraContextValue].GetDeviceInfo().GetModelName())
+        # Print the index and the model name of the camera.
+        # print("Camera ", cameraContextValue, ": ", self.cameras[cameraContextValue].GetDeviceInfo().GetModelName())
 
-            # Now, the image data can be processed.
-            # print("GrabSucceeded: ", grabResult.GrabSucceeded())
-            # print("SizeX: ", grabResult.GetWidth())
-            # print("SizeY: ", grabResult.GetHeight())
-            img = grabResult.GetArray()
-            grabResult.Release()
-            # print("Gray value of first pixel: ", img[0, 0])
-            # img = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2RGB)
+        # Now, the image data can be processed.
+        # print("GrabSucceeded: ", grabResult.GrabSucceeded())
+        # print("SizeX: ", grabResult.GetWidth())
+        # print("SizeY: ", grabResult.GetHeight())
+        img = grabResult.GetArray()
+        grabResult.Release()
+        # print("Gray value of first pixel: ", img[0, 0])
+        # img = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2RGB)
 
-            # If image is properly acquired, fix it (for the VT Camera)
-            if self.ip == settings.CAMERA_HZ_IP:
-                img = np.rot90(img, 2)
+        # If image is properly acquired, fix it (for the VT Camera)
+        if self.ip == settings.CAMERA_HZ_IP:
+            img = np.rot90(img, 2)
 
-            # Output status and return image
-            end_t = time.time()
-            # logger.debug("Image grabbing time: %.02f" % (end_t - start_t))
-            return img
+        # Output status and return image
+        end_t = time.time()
+        logger.debug("Image grabbing time: %.02f", (end_t - start_t))
+        return img
 
     def detach(self,):
         """Detach camera
         """
-        self._cam.DetachDevice()
+        cameras[self.cam_idx].DetachDevice()
 
     def setLight(self, status):
         """Set light on current camera (if applicable)
         We use GPIO here.
         See file:///Applications/pylon%20Programmer's%20Guide%20and%20API%20Reference.app/Contents/Resources/Html/class_pylon_1_1_c_basler_gig_e_camera.html
         """
-        self._cam.LineSelector.SetValue("Line3")
-        self._cam.LineInverter.SetValue(status)
+        cameras[self.cam_idx].LineSelector.SetValue("Line3")
+        cameras[self.cam_idx].LineInverter.SetValue(status)
 
     def saveImage(self, frame, name=None, ratio=1):
         # make filename like yyyy-mm-dd-hh-mm-ss-nn-cam_id.png
