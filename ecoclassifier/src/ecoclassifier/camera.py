772d53136522f722636b96437994e0769e17b210
Pierre-Julien Grizel <pjgrizel@numericube.com>
2019-06-25 15:02:53 +0200

    Multiple cam experiment

--- ecoclassifier/src/ecoclassifier/camera.py
+++ ecoclassifier/src/ecoclassifier/camera.py
@@ -33,15 +33,64 @@ from . import settings
 logger = logging.getLogger(__name__)
 
 # Initialize cameras
-devices = None
-cameras = None
-tlFactory = py.TlFactory.GetInstance()
-devices = tlFactory.EnumerateDevices()
-cameras = py.InstantCameraArray(len(devices))
-for i, cam in enumerate(cameras):
-    cam.Attach(tlFactory.CreateDevice(devices[i]))
-    logger.debug("Using device %s", cam.GetDeviceInfo().GetIpAddress())
-cameras.StartGrabbing(py.GrabStrategy_LatestImageOnly)
+# devices = None
+# cameras = None
+# tlFactory = py.TlFactory.GetInstance()
+# devices = tlFactory.EnumerateDevices()
+# # cameras = py.InstantCameraArray(len(devices))
+# for i, cam in enumerate(cameras):
+#     cam.Attach(tlFactory.CreateDevice(devices[i]))
+#     logger.debug("Using device %s", cam.GetDeviceInfo().GetIpAddress())
+# cameras.StartGrabbing(py.GrabStrategy_LatestImageOnly)
+
+
+class Cameras:
+    """Camera pair abstraction.
+    Taken from https://github.com/basler/pypylon/issues/83
+    """
+
+    def __init__(self):
+        pylon = py
+        tlFactory = pylon.TlFactory.GetInstance()
+
+        # Get all attached devices and exit application if no device is found.
+        devices = tlFactory.EnumerateDevices()
+        if len(devices) == 0:
+            raise pylon.RUNTIME_EXCEPTION("No camera present.")
+
+        # Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
+        self.cameras = pylon.InstantCameraArray(min(len(devices), 2))
+
+        # Create and attach all Pylon Devices.
+        for i, cam in enumerate(self.cameras):
+            cam.Attach(tlFactory.CreateDevice(devices[i]))
+            cam.RegisterConfiguration(
+                pylon.SoftwareTriggerConfiguration(),
+                pylon.RegistrationMode_ReplaceAll,
+                pylon.Cleanup_Delete,
+            )
+            # For demonstration purposes only, add a sample configuration event handler to print out information
+            # about camera use.t
+            cam.RegisterConfiguration(
+                ConfigurationEventPrinter(),
+                pylon.RegistrationMode_Append,
+                pylon.Cleanup_Delete,
+            )
+            # The image event printer serves as sample image processing.
+            # When using the grab loop thread provided by the Instant Camera object, an image event handler processing the grab
+            # results must be created and registered.
+            cam.RegisterImageEventHandler(
+                ImageEventPrinter(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete
+            )
+
+        # Start the grabbing using the grab loop thread, by setting the grabLoopType parameter
+        # to GrabLoop_ProvidedByInstantCamera. The grab results are delivered to the image event handlers.
+        # The GrabStrategy_OneByOne default grab strategy is used.
+        self.cameras.StartGrabbing(
+            py.GrabStrategy_LatestImageOnly, pylon.GrabLoop_ProvidedByInstantCamera
+        )
+        self.vt_camera = self.cameras[0]
+        self.hz_camera = self.cameras[1]
 
 
 class Camera:
